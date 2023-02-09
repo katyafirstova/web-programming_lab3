0f400a4823073439781a43e6534018f50ffdd2b0
katyafirstova <firstovakatya42@gmail.com>
2023-02-09 12:58:16 +0300

    update faces

--- src/main/webapp/resources/scripts/TargetDot.js
+++ src/main/webapp/resources/scripts/TargetDot.js
@@ -8,12 +8,15 @@ class TargetDot {
     }
 
     static create() {
+
         this.x = document.getElementById('plot-form:plot-valueX').value;
         this.y =  document.getElementById('plot-form:plot-valueY').value;
-        this.r =  document.getElementById('plot-form:plot-valueR').value
-        this.result = document.getElementById('plot-form:plot-check').value;
+        this.r =  document.getElementById('plot-form:plot-valueR').value;
+        this.result = document.getElementById('plot-form:plot-check').click();
+
         this.calculateColor()
         this.calculateCoordinates()
+
     }
 
     static calculateCoordinates() {
@@ -21,7 +24,7 @@ class TargetDot {
         const calcY = 300 - this.y * 200 / this.r
 
         let target = document.getElementById('target-dot');
-        target.setAttribute('r', '3')
+        target.setAttribute('r', 10)
         target.setAttribute('cx', String(calcX))
         target.setAttribute('cy', String(calcY))
         target.setAttribute('fill', this.color)
@@ -32,9 +35,9 @@ class TargetDot {
     }
 
     static calculateResult() {
-        let checkRectangle = this.x <= 0 && this.y >= 0 && this.x <= this.r/2 && this.y >= -this.r
-        let checkQuarterCircle = this.x >= 0 && this.y >= 0 && (this.x*this.x + this.y*this.y) <= this.r*this.r
-        let checkTriangle = this.x >= 0 && this.y <= 0 && (this.x*this.x + this.y*this.y) >= this.r*this.r;
+        let checkRectangle = this.x <= 0 && this.y >= 0 && this.x <= this.r / 2 && this.y >= -this.r
+        let checkQuarterCircle = this.x >= 0 && this.y >= 0 && (this.x * this.x + this.y * this.y) <= this.r * this.r
+        let checkTriangle = this.x >= 0 && this.y <= 0 && (this.x * this.x + this.y * this.y) >= this.r * this.r;
 
         this.result = (checkRectangle || checkQuarterCircle || checkTriangle).toString()
     }
